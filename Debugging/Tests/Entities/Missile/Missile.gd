0081f101a8f6e1e2fc68107ceeb648ceeb23b794
Juanxpeke <juanintendo.f11@gmail.com>
2025-01-20 00:30:38 -0300

    Added basic 2D missile test

---
+++ Debugging/Tests/Entities/Missile/Missile.gd
@@ -0,0 +1,52 @@
+extends CharacterBody2D
+## Docstring
+
+#region Signals
+#endregion Signals
+
+#region Enums
+#endregion Enums
+
+#region Constants
+#endregion Constants
+
+#region Exports Variables
+@export var movement_speed : float = 900.0 
+@export var rotation_speed : float = deg_to_rad(360.0)
+#endregion Exports Variables
+
+#region Public Variables
+#endregion Public Variables
+
+#region Private Variables
+#endregion Private Variables
+
+#region On Ready Variables
+#endregion On Ready Variables
+
+#region Built-in Virtual Methods
+func _ready() -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("move_left"):
+		rotate(-rotation_speed * delta)
+	if Input.is_action_pressed("move_right"):
+		rotate(rotation_speed * delta)
+	
+	var local_up_vector = global_transform.basis_xform(Vector2.UP)
+	velocity = local_up_vector * movement_speed
+	
+	move_and_slide()
+	
+	if get_slide_collision_count() > 0:
+		_explode()
+#endregion Built-in Virtual Methods
+
+#region Public Methods
+#endregion Public Methods
+
+#region Private Methods
+func _explode() -> void:
+	queue_free()
+#endregion Private Methods
