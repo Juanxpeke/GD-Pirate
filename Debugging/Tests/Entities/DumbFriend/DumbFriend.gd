d3fce42b0a9746ebe851a192bbc635e5af1be8f8
Juanxpeke <juanintendo.f11@gmail.com>
2025-01-21 20:52:38 -0300

    Added breakable tiles

--- Debugging/Tests/Entities/DumbFriend/DumbFriend.gd
+++ Debugging/Tests/Entities/DumbFriend/DumbFriend.gd
@@ -18,7 +18,7 @@ enum CharacterKey {
 
 #region Constants
 ## TODO
-const INITIAL_IDLE_TIME : float = 211.0
+const INITIAL_IDLE_TIME : float = 2.0
 ## TODO
 const HORIZONTAL_MOVEMENT_ACCELERATION_ON_FLOOR : float = 720.0
 ## TODO
@@ -30,11 +30,15 @@ const MAX_HORIZONTAL_MOVEMENT_SPEED_ON_FLOOR : float = 500.0
 ## TODO
 const MAX_HORIZONTAL_MOVEMENT_SPEED_ON_AIR : float = 1000.0
 ## TODO
+const MAX_VERTICAL_MOVEMENT_SPEED : float = 2000.0
+## TODO
 const MIN_RANDOM_KEY_TIME : float = 1.0
 ## TODO
 const MAX_RANDOM_KEY_TIME : float = 4.0
 ## TODO
-const STOMP_ACCELERATION : float = 4000.0
+const STOMP_HORIZONTAL_FRICTION : float = 2000.0
+## TODO
+const STOMP_VERTICAL_ACCELERATION : float = 4000.0
 ## TODO
 const CHARACTER_KEYS_DATA = {
 	CharacterKey.GO_LEFT : {
@@ -66,12 +70,14 @@ var _character_key : CharacterKey = CharacterKey.NONE
 
 #region On Ready Variables
 @onready var _grappling_hook_pivot : Node2D = %GrapplingHookPivot
+@onready var _stomp_area           : Area2D = %StompArea
 @onready var _random_key_timer     : Timer  = %RandomKeyTimer
 @onready var _random_key_label     : Label  = %RandomKeyLabel
 #endregion On Ready Variables
 
 #region Built-in Virtual Methods
 func _ready() -> void:
+	_assert_parameters()
 	_initial_connections()
 	_initial_setup()
 
@@ -100,8 +106,10 @@ func _physics_process(delta: float) -> void:
 	velocity += get_gravity() * delta
 	
 	if _character_key == CharacterKey.STOMP:
-		velocity.x = 0
-		velocity.y += STOMP_ACCELERATION * delta
+		velocity.x = move_toward(velocity.x, 0.0, delta * STOMP_HORIZONTAL_FRICTION)
+		velocity.y += STOMP_VERTICAL_ACCELERATION * delta
+	
+	velocity.y = clamp(velocity.y, -MAX_VERTICAL_MOVEMENT_SPEED, MAX_VERTICAL_MOVEMENT_SPEED)
 	
 	move_and_slide()
 #endregion Built-in Virtual Methods
@@ -110,12 +118,28 @@ func _physics_process(delta: float) -> void:
 #endregion Public Methods
 
 #region Private Methods
+#region Assertions
+func _assert_parameters() -> void:
+	assert(not _stomp_area.monitoring) # NOTE: Should be monitoring only when stomp key is pressed
+#endregion Assertions
 #region Callbacks
+func _on_stomp_area_shape_entered(body_rid : RID, body : Node2D, _body_shape_index : int, _local_shape_index : int) -> void:
+	if body is DumbTileMapLayer:
+		var dumb_tile_map_layer : DumbTileMapLayer = body
+		
+		var coords := dumb_tile_map_layer.get_coords_for_body_rid(body_rid)
+		
+		var tile_data := dumb_tile_map_layer.get_cell_tile_data(coords)
+		
+		if tile_data.get_custom_data("Breakable"):
+			dumb_tile_map_layer.erase_cell(coords)
+
 func _on_random_key_timer_timeout() -> void:
 	_set_new_character_key()
 #endregion Callbacks
 #region Initialization
 func _initial_connections() -> void:
+	_stomp_area.body_shape_entered.connect(_on_stomp_area_shape_entered)
 	_random_key_timer.timeout.connect(_on_random_key_timer_timeout)
 
 func _initial_setup() -> void:
@@ -144,6 +168,11 @@ func _set_new_character_key(exclude_current : bool = true) -> void:
 			break
 	
 	_random_key_label.text = CHARACTER_KEYS_DATA[_character_key].name
+	
+	if _character_key == CharacterKey.STOMP:
+		_stomp_area.monitoring = true
+	else:
+		_stomp_area.monitoring = false
 	
 	_random_key_timer.start(_rng.randf_range(MIN_RANDOM_KEY_TIME, MAX_RANDOM_KEY_TIME))
 #endregion Private Methods
