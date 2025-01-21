f8611564e3303afd5f3326a8321b4f0f4d22e872
Juanxpeke <juanintendo.f11@gmail.com>
2025-01-21 00:34:23 -0300

    Added hook pointer

--- Debugging/Tests/Entities/DumbFriend/GrapplingHook.gd
+++ Debugging/Tests/Entities/DumbFriend/GrapplingHook.gd
@@ -12,7 +12,7 @@ extends Node2D
 
 #region Exports Variables
 @export var character : CharacterBody2D = null
-@export var hook_impulse_magnitude : float = 800.0
+@export var hook_impulse_magnitude : float = 960.0
 #endregion Exports Variables
 
 #region Public Variables
@@ -22,7 +22,8 @@ extends Node2D
 #endregion Private Variables
 
 #region On Ready Variables
-@onready var ray_cast : RayCast2D = %RayCast
+@onready var _ray_cast : RayCast2D = %RayCast
+@onready var _pointer : Sprite2D = %Pointer
 #endregion On Ready Variables
 
 #region Built-in Virtual Methods
@@ -30,11 +31,17 @@ func _ready() -> void:
 	assert(character)
 
 func _process(delta : float) -> void:
-	pass
+	if _ray_cast.is_colliding():
+		if not _pointer.visible:
+			_pointer.show()
+		
+		_pointer.global_position = _ray_cast.get_collision_point()
+	elif _pointer.visible:
+		_pointer.hide()
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("left_click") and ray_cast.is_colliding():
-		var collision_point := ray_cast.get_collision_point()
+	if event.is_action_pressed("left_click") and _ray_cast.is_colliding():
+		var collision_point := _ray_cast.get_collision_point()
 		
 		var impulse_vector := collision_point - character.global_position
 		var impulse_direction := impulse_vector.normalized()
